b783e6ca7cc9ebb9fcbcbd43860d38239b8bc3b6
Axenmagnus <Axenmagnus@users.noreply.github.com>
2022-10-12 14:41:18 +0200

    Updtea

--- NN.py
+++ NN.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  7 11:19:58 2022
 
-@author: magnusaxen
-"""
+
+
+
+
+#%%
 from keras import models, layers, utils, backend as K
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 #import shap
 import numpy as np
 import matplotlib.pyplot as plt
@@ -32,29 +32,91 @@ data_0=train_data.loc[train_data['Label'] == 0]
 
 #data_0.hist(bins=30)#, figsize=(15, 10))
 #data_1.hist(bins=30)#, figsize=(15, 10))
-Y=train_data["Label"]
+train_data, test_data = train_test_split(train_data, test_size=0.2)
+Y_Train=train_data["Label"]
+Y_Test=test_data["Label"]
 #X=train_data.drop(columns='key')
-X = pd.get_dummies(data=train_data, columns=['key'])
-X=X.drop(columns=['Label'])
+#normalization
+
+#X_Train = pd.get_dummies(data=train_data, columns=['key'])
+
+X_Train=train_data
+X_Train = X_Train.drop(columns=['key'])
+X_Train = X_Train.drop(columns=['mode'])
+X_Train = X_Train.drop(columns=['instrumentalness'])
+
+X_Train =(X_Train-X_Train.mean())/X_Train.std() #<- this one works well
+X_Train=X_Train.drop(columns=['Label'])
+
+
+#X_Test = pd.get_dummies(data=test_data, columns=['key'])
+
+X_Test = test_data
+X_Test =X_Test.drop(columns=['key'])
+X_Test =X_Test.drop(columns=['mode'])
+X_Test = X_Test.drop(columns=['instrumentalness'])
+
+X_Test =(X_Test-X_Test.mean())/X_Test.std() #<- this one works well
+X_Test=X_Test.drop(columns=['Label'])
+#X=X.drop(columns=['Label'])
 #X=X.drop(columns='Label')
+
+#X=X.drop(columns=['key'])
 # %% Done with data-Prepp
 
 from keras.models import Sequential
 from keras.layers import Dense
 
+#model = Sequential()
+#model.add(Dense(8,input_dim=len(X_Train.columns),activation="relu"))
+#model.add(Dense(10,activation="relu"))
+#model.add(Dense(6,activation="relu"))
+#model.add(Dense(4,activation="relu"))
+##model.add(Dense(5,activation="relu"))
+##model.add(Dense(6,activation="relu"))
+##model.add(Dense(5,activation="relu"))
+#model.add(Dense(1,activation="sigmoid"))
+
+#model.summary()
+# %%
+from tensorflow import keras
+# learnn=[0.1,0.05,0.01,0.005,0.0001,0.00005,0.00001]
+# for learn in learnn:
+#     #opt = keras.optimizers.Adam(learning_rate=0.001)
+#     model = Sequential()
+#     model.add(Dense(8,input_dim=len(X_Train.columns),activation="relu"))
+#     #model.add(Dense(10,activation="relu"))
+#     model.add(Dense(6,activation="relu"))
+#     model.add(Dense(4,activation="relu"))
+#     #model.add(Dense(5,activation="relu"))
+#     #model.add(Dense(6,activation="relu"))
+#     #model.add(Dense(5,activation="relu"))
+#     model.add(Dense(1,activation="sigmoid"))
+
+#     opt = keras.optimizers.Adam(learning_rate=learn)
+#     model.compile(loss="binary_crossentropy",optimizer=opt,metrics=["accuracy"])
+    
+#     model.fit(x=X_Train,y=Y_Train,epochs=50)#,verbose=1)
+    
+#     print(model.evaluate(X_Test,(Y_Test)))
+#%%
+#Found the former optimal learning rate
 model = Sequential()
-model.add(Dense(10,input_dim=len(X.columns),activation="relu"))
-model.add(Dense(8,activation="relu"))
-model.add(Dense(4,activation="relu"))
+model.add(Dense(9,input_dim=len(X_Train.columns),activation="relu"))
+model.add(Dense(9,activation="relu"))
+#model.add(Dense(6,activation="relu"))
+#model.add(Dense(10,activation="relu"))
+#model.add(Dense(5,activation="relu"))
+model.add(Dense(9,activation="relu"))
+#model.add(Dense(5,activation="relu"))
 model.add(Dense(1,activation="sigmoid"))
 
-model.summary()
-# %%
+opt = keras.optimizers.Adam(learning_rate=0.005)
+model.compile(loss="binary_crossentropy",optimizer=opt,metrics=["accuracy"])
 
-model.compile(loss="binary_crossentropy",optimizer="rmsprop",metrics=["accuracy"])
+model.fit(x=X_Train,y=Y_Train,epochs=40)#,verbose=1)
 
-model.fit(x=X,y=Y,epochs=200,verbose=1)
-
+print(model.evaluate(X_Test,(Y_Test)))
 
 
 
